7d29b17be19e6bbf96900e7144546905d93c5f42
Zach-woller <821001@mystma.org>
2024-09-20 09:52:19 -0500

    Top 5 movies

---
+++ Assingments/Top 5 moives.py
@@ -0,0 +1,6 @@
+print ("these are my top five movies:")
+print ("1. Whiplash")
+print ("2. La La Land")
+print ("3. Star Wars Empire Strikes Back")
+print ("4. Scot Pillgrim Vs The World")
+print ("5. Star Wars Revenge Of The Sith")
