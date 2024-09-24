8fe948c420a7a86026ef92dcedce686159547568
Zach-woller <821001@mystma.org>
2024-09-24 09:47:47 -0500

    Sept. 24

---
+++ Assingments/Animal Survey.py
@@ -0,0 +1,2 @@
+favorite_animal =input (" what is you favorite animal\n>")
+input_1 =input ("why are they your favorite animal\n>")
---
+++ Unit 2/pracitce sept 2-.py
@@ -0,0 +1,3 @@
+user_input = input ("type out a number\n>")
+user_input = int(user_input)
+print(user_input * 10)
