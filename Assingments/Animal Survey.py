8a4c3a2fbee69191b8ef4deb871d1d55cba6752f
Zach-woller <821001@mystma.org>
2024-09-25 09:29:34 -0500

    Animal survey notes

--- Assingments/Animal Survey.py
+++ Assingments/Animal Survey.py
@@ -1,6 +1,6 @@
-favorite_animal =input (" What is you favorite animal?\n>")
-input_1 =input ("Why are they your favorite animal?\n>")
-input_2 =input ("Whats you favorite thing about them?\n>")
-input_3 =input ("Where are they from?\n>")
-input_4 =input ("What do they eat?\n>")
-print ("Your favorite animal is " + favorite_animal + "." + " they are you favorite because " + input_1 + "." + " They are from " + input_3 + " ," "and they eat " + input_4 + "." + " your favorite thing about them is " + input_2 + "." )
+favorite_animal =input (" What is you favorite animal?\n>") #Input
+input_1 =input ("Why are they your favorite animal?\n>") #Input
+input_2 =input ("Whats you favorite thing about them?\n>") #Input
+input_3 =input ("Where are they from?\n>") #Input
+input_4 =input ("What do they eat?\n>") #Input
+print ("Your favorite animal is " + favorite_animal + "." + " they are you favorite because " + input_1 + "." + " They are from " + input_3 + " ," "and they eat " + input_4 + "." + " your favorite thing about them is " + input_2 + "." ) #prints the inputs into a summary
