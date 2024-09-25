cddc53d1dc781547e65842a2bcb4b8c26a86220c
Zach-woller <821001@mystma.org>
2024-09-25 09:48:47 -0500

    converting

---
+++ Assingments/Converting data types.py
@@ -0,0 +1,27 @@
+# Complete your Converting Data Types activity here
+
+# Successfully convert all of the following variables to another type and print the result
+# If the conversion prints without errors, you did the conversion correctly
+
+a = 115         #int -> string
+a =str(a)
+b = 3.14        #float -> string
+b =str(b)
+c = "68"        #string -> int
+c =int(c)
+d = "True"      #string -> boolean
+d =bool(d)
+e = True        #boolean -> string
+e =str(e)
+f = False       #boolean -> string
+f =str(f)
+g = '10110111'  #byte -> int
+g =int(g)
+h = "2.54"      #string -> float
+h =float(h) 
+i = 100         #int -> float
+i =float(i)
+j = 10.0        #float -> int
+j =int(j)
+k = 254         #int -> byte
+k =bin(k)
