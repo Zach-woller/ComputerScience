6aa8287ba98a711d24ebedf50fa77682ad925c73
Zach-woller <821001@mystma.org>
2024-10-03 09:47:55 -0500

    test

---
+++ Assingments/unit2test.py
@@ -0,0 +1,22 @@
+word_1 = input("input the 1st word\n>")     #1
+word_2 = input("input the 2nd word\n>")
+word_3 = input("input the 3rd word\n>")
+print(word_1 + word_2 + word_3)
+def add_three():                            #2         
+    x = input("input the 1st number\n>")
+    x = int(x)
+    y = input("input the 2nd number\n>")
+    y = int(y)
+    z = input("input the 3rd number\n>")
+    z = int(z) 
+    print(str(x) + " + " + str(y) + " + " + str(z) + " = " + str(x + y + z))
+add_three()
+def add_three():                           #3
+    x = input("input the 1st number\n>")
+    x = int(x)
+    y = input("input the 2nd number\n>")
+    y = int(y)
+    z = input("input the 3rd number\n>")
+    z = int(z) 
+    print(str(x) + " + " + str(y) + " + " + str(z) + " = " + str(x + y + z))
+add_three()
---
+++ Unit 2/Review.py
@@ -0,0 +1,11 @@
+username = input("what is you name\n>")
+
+#functions
+print("Hello, world!")          #hello world is a parameter
+input("whats your name?\n>")    #is a escape character
+
+str()
+int()
+float()
+bin()
+bool()
