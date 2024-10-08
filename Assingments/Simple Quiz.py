ddcd16abe9f4846e11c1653ed67182c937267360
Zach-woller <821001@mystma.org>
2024-10-08 09:47:23 -0500

    Oct 8

---
+++ Assingments/Simple Quiz.py
@@ -0,0 +1,33 @@
+def tally_score():
+    score = 0
+    if question_1 == 5:
+        score = score +1
+    if question_2 == 17:
+        score = score +1
+    if question_3 == 6:
+        score = score +1
+    if question_4 == 8:
+        score = score +1
+    if question_5 < 9:
+        score = score +1
+    print(score)
+score = 0
+question_1 = int(input("What is 7 - 2\n>"))
+question_2 = int(input("What is 10 + 7\n>"))
+question_3 = int(input("What is 3 * 2\n>"))
+question_4 = int(input("What is 4 * 2\n>"))
+question_5 = int(input("Name a number less then 9\n>"))
+def tally_score():
+    score = 0
+    if question_1 == 5:
+        score = score +1
+    if question_2 == 17:
+        score = score +1
+    if question_3 == 6:
+        score = score +1
+    if question_4 == 8:
+        score = score +1
+    if question_5 < 9:
+        score = score +1
+    print("Your score is " + score)
+tally_score()
---
+++ Unit 3/string_functions.py
@@ -0,0 +1,9 @@
+#   .Lower()
+input_answer = ("Lord of The Rings")
+input_answer = input_answer.lower()
+real_answer = ("lord of the rings")
+print(input_answer == real_answer)
+
+
+# .upper
+# makes a string upper case
