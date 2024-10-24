95b22b656ca8f8e2743bf0e62e3e65ca1f209006
Zach-woller <821001@mystma.org>
2024-10-24 09:48:09 -0500

    oct 24

--- Assingments/textadventure.py
+++ Assingments/textadventure.py
@@ -1,14 +1,35 @@
+def death():
+    print ("You  have died!")
+    print("continue?")
+    print("1. Yes")
+    print("2. No")
+
+    death_health_choice = input("> ")
+
+    if death_health_choice == "1":
+        start_menu()
+    elif death_health_choice == "2":
+        print("ending program")
+    else:
+        print("ERROR Unexpected input")
+        death()
+
+
 health = 30 
 if health == 0:
-    death_health()
+    death()
 
-    def death_health():
-        print
 
-def start_nate():
+def start_nate():       #start game with choosen character
     print
 
-def nate(): 
+def start_rose():       #start game with choosen character
+    print  
+
+def start_robbie():     #start game with choosen character
+    print
+
+def nate():             #tells player what the character has and who they are
     print("Nate is a 45 year old retired firefighter who resides in a small apartment in the Bronx. He has a backpack with 3 days of canned food and 5 days of water. He owns a car and survival hatchet.")
     print ("1. start journey")
     print ("2. choose different character")
@@ -23,14 +44,38 @@ def nate():
         print("ERROR Unexpected input")
         nate()
 
-def rose():
-    print("Rose is a 22 year old barista who works at starbucks. She owns a apartment in Manhattan her purse has 2 days of canned food")
+def rose():             #tells player what the character has and who they are
+    print("Rose is a 22 year old barista who works at starbucks. She owns a apartment in Manhattan. her purse has 2 days of canned food and water.")
+    print ("1. start journey")
+    print ("2. choose different character")
 
-def robbie():
-    print
+    character_2 = input("> ")
 
-def jonas():
-    print
+    if character_2 == "1":
+        start_rose()
+    elif character_2 == "2":
+        character_menu()
+    else:
+        print("ERROR Unexpected input")
+        rose()
+
+def robbie():           #tells player what the character has and who they are
+    print("Robbie is a 26 year old artist living Brooklyln. He is living out of his car he has a plasic bag with 5 days of food and water. he owns a  swiss army knife.")
+    print ("1. start journey")
+    print ("2. choose different character")
+
+    character_2 = input("> ")
+
+    if character_2 == "1":
+        start_robbie()
+    elif character_2 == "2":
+        character_menu()
+    else:
+        print("ERROR Unexpected input")
+        robbie()
+
+def jonas():            #tells player what the character has and who they are
+    print("Jonas is a 19 year old college student. he lives with with his mom and dad in a quaint little neighborhood in Queens. ")
 
 def character_menu():
     print("The year is 1997. A zombie virus has taken the world by storm. it is unknown how it came to but its here now. You are living in new york.  You need to leave")
@@ -39,6 +84,7 @@ def character_menu():
     print("2. Rose")
     print("3. Robbie")
     print("4. Jonas")
+    print("5. return to menu")
 
     character = input("> ")
     
@@ -50,6 +96,8 @@ def character_menu():
         robbie()
     elif character == "4":
         jonas()
+    elif character == "5":
+        start_menu()
     else:
         print("ERROR Unexpected input")
         character_menu()
