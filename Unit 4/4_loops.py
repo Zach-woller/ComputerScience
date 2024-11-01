3f2248abe62ee815b84afe499bf0d07ae35d4709
Zach-woller <821001@mystma.org>
2024-11-01 09:53:39 -0500

    Nov 1

---
+++ Unit 4/4_loops.py
@@ -0,0 +1,18 @@
+for i in range(50,101):
+    print(i)
+    print("yo")
+
+top_foods = ["Pasta", "pizza", "mac n cheese"]
+
+for food in top_foods:
+    print(food)
+
+
+    shoping = ["milk", "eggs", "bread", "butter", "apples"]
+
+for shop in shoping:
+    print(shop)
+
+import random
+numbers = [random.randint(1,101),random.randint(1,101),random.randint(1,101),random.randint(1,101),random.randint(1,101),random.randint(1,101)]
+print(numbers)
--- Unit 4/fav_game.py
+++ Unit 4/fav_game.py
@@ -1,2 +1,3 @@
 Games_list = []
 
+game_1 = input("What is you 1st favorite game")
