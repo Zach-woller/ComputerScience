4b762dd713ab0d5ad2a12e06d9dbe91e5e60b69f
Zach-woller <821001@mystma.org>
2024-11-19 09:27:55 -0600

    nov 19

---
+++ Circuit Playground/Thermometer.py
@@ -0,0 +1,29 @@
+# We first need to import board specific tools
+# Every project for this board will need this statement
+
+from adafruit_circuitplayground import cp
+
+import random
+import time
+
+while True:
+    temp_c = cp.temperature
+    temp_f = (temp_c * 9 / 5) + 32
+    if temp_f < 78:
+        cp.pixels[0](0,0,1)
+    if temp_f > 78:
+        cp.pixels[1](0,0,1)
+    if temp_f > 79:
+        cp.pixels[2](0,0,1)
+    if temp_f > 80:
+        cp.pixels[3](1,1,0)
+    if temp_f > 81:
+        cp.pixels[4](1,1,0)
+    if temp_f > 82:
+        cp.pixels[5](1,1,0)
+    if temp_f > 83:
+        cp.pixels[6](1,1,0)
+    if temp_f > 84:
+        cp.pixels[7](1,0,0)
+    if temp_f > 85:
+        cp.pixels[8](1,0,0)
